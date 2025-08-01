8e3308850c82b77faaccc661cb38f8f52ae49e70
Sujal <poojarisujal03@gmail.com>
2025-08-01 17:47:27 +0530

    added contact

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import About from './components/About'
 import Navbar from './components/Navbar'
 import Experience from './components/Experience'
 import Projects from './components/Projects'
-import Contact from './components/contact'
+import Contact from './components/Contact'
+
 
 const App = () => {
   return (
